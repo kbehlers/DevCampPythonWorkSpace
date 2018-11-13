4866e559902b3c60d87f363c42af62bcf2b35dca
kbehlers <kemais.ehlers@gmail.com>
2018-11-13 09:26:41 -0700

    new entry and edit entry learning log.

--- django-pizza/pizzeria/urls.py
+++ django-pizza/pizzeria/urls.py
@@ -4,7 +4,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pizzeria'
 urlpatterns = [
     #Home page
     path('', views.index, name='index'),
+
+    #Pizza List Page
+    path('pizzas/', views.pizzas, name='pizzas'),
+
+    #Detail page for toppings
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
 ]
--- django-pizza/pizzeria/views.py
+++ django-pizza/pizzeria/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
+from .models import Pizza
+
 # Create your views here.
 
 def index(request):
     """The home page for the pizzeria"""
     return render(request, 'pizzeria/index.html')
+
+def pizzas(request):
+    """The list of pizzas page"""
+    pizzas = Pizza.objects.order_by('id')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizzeria/pizzas.html', context)
+
+def pizza(request, pizza_id):
+    """Show toppings for a pizza"""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('id')
+    print(toppings)
+    context = {'pizza': pizza, 'toppings': toppings }
+    return render(request, 'pizzeria/pizza.html', context)
