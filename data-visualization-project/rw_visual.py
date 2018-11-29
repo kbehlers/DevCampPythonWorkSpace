954f09bd3020dc230419a6213a8d301ed0f89b30
kbehlers <kemais.ehlers@gmail.com>
2018-11-29 14:31:34 -0700

    plot random walk without axes and with set figure size

--- data-visualization-project/rw_visual.py
+++ data-visualization-project/rw_visual.py
@@ -3,9 +3,27 @@ import matplotlib.pyplot as plt
 #My modules
 from random_walk import RandomWalk
 
-#Make a random walk and plot points
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    #Make a random walk and plot points
+    rw = RandomWalk(50000)
+    rw.fill_walk()
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    #Set the size of the plotting window
+    plt.figure(figsize=(10,6))
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+
+    #Emphasize the first and last points
+    plt.scatter(0, 0, c='green', edgecolors='gray', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='black', s=100)
+
+    #Remove the axes
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
