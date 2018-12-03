0b3cd377c695b41614ee089b121f3b707a166e03
kbehlers <kemais.ehlers@gmail.com>
2018-12-03 11:44:30 -0700

    standardized y axis for high low plots

--- data-visualization-project/highs_lows_alaska.py
+++ data-visualization-project/highs_lows_alaska.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 #Read dates, highs, and lows temperatures from file
 # filename = 'data-visualization-project\sitka_weather_07-2014.csv'
-filename = 'data-visualization-project\sitka_weather_2014.csv'
+filename = 'C:/Users/Copy/Documents/DevCampPythonWorkSpace/data-visualization-project/sitka_weather_2014.csv'
 with open(filename) as f_obj:
     reader = csv.reader(f_obj)
     header_row = next(reader)
@@ -31,8 +31,9 @@ plt.plot(dates, highs, c='red', alpha=0.5)
 plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='purple', alpha=0.1)
 
-#Format plol
+#Format plot
 plt.title("Daily high and low temperatures - 2014", fontsize=24)
+plt.ylim(-50, 150) 
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
--- data-visualization-project/highs_lows_california.py
+++ data-visualization-project/highs_lows_california.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 #Read dates, highs, and lows temperatures from file
 # filename = 'data-visualization-project\sitka_weather_07-2014.csv'
-filename = 'data-visualization-project\sitka_weather_2014.csv'
+filename = 'data-visualization-project\death_valley_2014.csv'
 with open(filename) as f_obj:
     reader = csv.reader(f_obj)
     header_row = next(reader)
@@ -15,14 +15,16 @@ with open(filename) as f_obj:
 
     dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[3])
-        lows.append(low)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 
 #Plot temperatures
@@ -32,7 +34,8 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='purple', alpha=0.1)
 
 #Format plol
-plt.title("Daily high and low temperatures - 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014\nDeath Valley, CA", fontsize=24)
+plt.ylim(-50, 150) 
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
