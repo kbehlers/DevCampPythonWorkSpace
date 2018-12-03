92cc7e605ae8a8861292753b2a5c7a06f15dbfd6
kbehlers <kemais.ehlers@gmail.com>
2018-12-03 11:18:53 -0700

    high and lows alaska plot

---
+++ data-visualization-project/different_dice.py
@@ -0,0 +1,32 @@
+from die import Die
+import pygal
+
+#Create a D6
+die_1 = Die()
+die_2 = Die(10)
+
+#Roll and store results
+results = []
+for roll_num in range(1000000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+#Analyze results
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+
+for value in range(2, max_result+1):
+    #Get the count of each face value
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+#Visualize the results
+hist = pygal.Bar()
+
+hist.title = "Results of rolling a D6 and a D10 a bunch of times"
+hist.x_labels = [str(x) for x in range(2,max_result+1)]
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6 + 10', frequencies)
+hist.render_to_file('different_dice_visual.svg')
---
+++ data-visualization-project/highs_lows_alaska.py
@@ -0,0 +1,41 @@
+import csv
+from datetime import datetime
+from matplotlib import pyplot as plt
+
+
+#Read dates, highs, and lows temperatures from file
+# filename = 'data-visualization-project\sitka_weather_07-2014.csv'
+filename = 'data-visualization-project\sitka_weather_2014.csv'
+with open(filename) as f_obj:
+    reader = csv.reader(f_obj)
+    header_row = next(reader)
+
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
+        high = int(row[1])
+        highs.append(high)
+
+        low = int(row[3])
+        lows.append(low)
+
+
+#Plot temperatures
+fig = plt.figure(dpi=128, figsize=(10,6))
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='purple', alpha=0.1)
+
+#Format plol
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
