e139bbb92261d3a527ca1e22118aac8459b5bfab
kbehlers <kemais.ehlers@gmail.com>
2018-12-10 14:21:00 -0700

    hide future dated questions. test coverage for question index

--- django-official-tutorial/polls/views.py
+++ django-official-tutorial/polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -45,7 +46,7 @@ class IndexView(generic.ListView):
     #get_queryset is a built-in for the class, but you can override for custom returns and filtering
     def get_queryset(self):
         """Return the last five published questions"""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
