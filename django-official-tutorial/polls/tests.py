f03f1d3a5617c47436ac99bb78c0a99ca8c4d6ab
kbehlers <kemais.ehlers@gmail.com>
2018-12-10 11:16:03 -0700

    Question was published recently test and bug fix added

--- django-official-tutorial/polls/tests.py
+++ django-official-tutorial/polls/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+
+from .models import Question
 
 # Create your tests here.
+
+class QuestionModelTests(TestCase):
+
+    # Just like python testing, methods should begin with 'test_'
+    def test_was_published_recently_with_future_question(self):
+        """ was_published_recently() returns False for questions whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
